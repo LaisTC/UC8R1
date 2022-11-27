2efc81aaf89db29e4fbf0c050a8b3fbfcfa51968
¨laistc2@gmail.com¨ <laistc2@gmail.com>
2022-11-27 18:28:11 -0300

    ¨ER1UC8¨

---
+++ sistema.js
@@ -0,0 +1,25 @@
+var pesoPeca = 200;
+
+if (pesoPeca < 100) {
+    console.log("A peça deve pesar no mínimo 100g")
+} else {
+    console.log("A peça possui peso adequado")
+}
+
+var listadePecas = ["Amortecedor","Disco de freio","Motor"]
+
+if(listadePecas.leight < 10) {
+    console.log(" é possivel cadastrar mais pecas")
+}
+else{
+    console.log("não e possivel cadastrar mais pecas")
+}
+
+var Nomepeca = "Motor"
+
+if(Nomepeca.leight >3){
+    console.log("nome valido")
+}
+else{
+    console.log("coloque um nome valido")
+}
